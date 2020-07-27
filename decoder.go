c2b57e5bbf794947e249953ba5ddcd927f406b26
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-07-27 11:58:03 -0500

    Return error if an error ocurrs dont panic

--- decoder.go
+++ decoder.go
@@ -3,19 +3,22 @@ package WhatMac
 import (
 	"bytes"
 	"io/ioutil"
-	"log"
 
 	"howett.net/plist"
 )
 
 var MacOSInfo macOSVersion
 
-func Decode() {
+func Decode() error {
 	initialFile, err := ioutil.ReadFile("/System/Library/CoreServices/SystemVersion.plist")
 	fileToReader := bytes.NewReader(initialFile)
 	if err != nil {
-		log.Panic(err)
+		return err
 	}
 	decoder := plist.NewDecoder(fileToReader)
 	err = decoder.Decode(&MacOSInfo)
+	if err != nil {
+		return err
+	}
+	return nil
 }
